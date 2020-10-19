450fe6ff1e547bc85ef8c29625bd07d7bd641f72
TAYAGCH <tayagch@oocl.com>
2020-10-19 15:42:26 +0800

    [word-frequency][christian]refactor: delete useless comments and old codes

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -12,24 +12,6 @@ public class WordFrequencyGame {
         } else {
 
             try {
-
-//                //split the input string with 1 to n pieces of spaces
-//                String[] arr = inputStr.split(WHITE_SPACES);
-//
-//                List<WordInfos> wordInfosList = new ArrayList<>();
-//                for (String s : arr) {
-//                    WordInfos wordInfos = new WordInfos(s, 1);
-//                    wordInfosList.add(wordInfos);
-//                }
-//
-//                //get the map for the next step of sizing the same word
-//                Map<String, List<WordInfos>> map =getListMap(wordInfosList);
-//
-//                List<WordInfos> list = new ArrayList<>();
-//                for (Map.Entry<String, List<WordInfos>> entry : map.entrySet()){
-//                    WordInfos wordInfos = new WordInfos(entry.getKey(), entry.getValue().size());
-//                    list.add(wordInfos);
-//                }
                 List<WordInfos> wordInfosList = calculateWordFrequency(inputStr);
 
                 wordInfosList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
@@ -57,26 +39,4 @@ public class WordFrequencyGame {
         }
         return wordInfos;
     }
-
-
-    private Map<String,List<WordInfos>> getListMap(List<WordInfos> wordInfosList) {
-        Map<String, List<WordInfos>> map = new HashMap<>();
-        for (WordInfos wordInfos : wordInfosList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfos.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfos);
-                map.put(wordInfos.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfos.getValue()).add(wordInfos);
-            }
-        }
-
-
-        return map;
-    }
-
-
 }
