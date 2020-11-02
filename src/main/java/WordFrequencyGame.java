285a600ec54265e79f187cab074037216e6636a6
TAYAGCH <tayagch@oocl.com>
2020-11-02 23:44:12 +0800

    test2

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,25 +8,25 @@ public class WordFrequencyGame {
 
     public String getResult(String sentence){
 
-        List<WordInfos> wordInfosList = calculateWordFrequency(sentence).stream()
-                .sorted(Comparator.comparing(WordInfos::getWordCount))
+        List<WordInfo> wordInfos = calculateWordFrequency(sentence).stream()
+                .sorted(Comparator.comparing(WordInfo::getWordCount))
                 .collect(Collectors.toList());
 
-        return joinWords(wordInfosList);
+        return joinWords(wordInfos);
     }
 
-    private String joinWords(List<WordInfos> wordInfosList) {
-        return wordInfosList.stream()
-                .map(wordInfos -> String.format("%s %d", wordInfos.getValue(), wordInfos.getWordCount()))
+    private String joinWords(List<WordInfo> wordInfoList) {
+        return wordInfoList.stream()
+                .map(wordInfo -> String.format("%s %d", wordInfo.getValue(), wordInfo.getWordCount()))
                 .collect(Collectors.joining("\n"));
     }
 
-    private List<WordInfos> calculateWordFrequency(String sentence){
+    private List<WordInfo> calculateWordFrequency(String sentence){
         List<String> words = Arrays.asList(sentence.split(WHITE_SPACES));
         return words.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
-                .map(entrySet -> new WordInfos(entrySet.getKey(),Math.toIntExact(entrySet.getValue())))
+                .map(entrySet -> new WordInfo(entrySet.getKey(),Math.toIntExact(entrySet.getValue())))
                 .collect(Collectors.toList());
     }
 }
